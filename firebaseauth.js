350ebeccc1e5d5251f670bbb11d03c0bd281b1e6
Safeeullah294 <safeeullah.ahsan@gmail.com>
2025-06-05 17:22:45 +0500

    Add files via upload

---
+++ firebaseauth.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDrrFVcKD3zHbholf43-IcDQK0fvqN36zc",
+   authDomain: "again-6b773.firebaseapp.com",
+   projectId: "again-6b773",
+   storageBucket: "again-6b773.firebasestorage.app",
+   messagingSenderId: "69481883320",
+   appId: "1:69481883320:web:155d7745b0dec42ebb364d"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
